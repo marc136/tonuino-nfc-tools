18e0e9a666deb94502c8e0222334098ae5480d0c
Marc Walter <walter.marc@outlook.com>
2019-04-27 17:11:21 +0200

    Refactor data types

--- app/src/main/java/com/example/myapplication/CardData.kt
+++ app/src/main/java/com/example/myapplication/CardData.kt
@@ -1,7 +1,11 @@
 package com.example.myapplication
 
+@ExperimentalUnsignedTypes
+private val TonuinoCookie = ubyteArrayOf(1u, 2u, 3u, 4u, 5u) // TODO set correct cookie
+
+@ExperimentalUnsignedTypes
 class CardData(
-    var cookie: UByteArray,
+    var cookie: UByteArray = TonuinoCookie,
     var version: UByte = 0x01.toUByte(),
-    val folderSettings: NfcFolderSettings
+    val folderSettings : NfcFolderSettings = NfcFolderSettings()
 )
--- app/src/main/java/com/example/myapplication/NfcFolderSettings.kt
+++ app/src/main/java/com/example/myapplication/NfcFolderSettings.kt
@@ -3,8 +3,8 @@ package com.example.myapplication
 // This name is used in Tonuino 2.1 (dev)
 @ExperimentalUnsignedTypes
 class NfcFolderSettings(
-    var folder: UByte,
-    var mode: UByte,
-    var special: UByte = UByte.MIN_VALUE,
-    var special2: UByte = UByte.MIN_VALUE
+    var folder: UByte = 1u,
+    var mode: UByte = 1u,
+    var special: UByte = 0u,
+    var special2: UByte = 0u
 )
