57fb1dbdb96e771d81b8c36be9d582275d83c9bc
Marc Walter <walter.marc@outlook.com>
2020-03-17 15:45:02 +0100

    Read NFC tags on bulkwrite activity

--- app/src/main/java/de/mw136/tonuino/ui/BulkWriteActivity.kt
+++ app/src/main/java/de/mw136/tonuino/ui/BulkWriteActivity.kt
@@ -1,17 +1,24 @@
 package de.mw136.tonuino.ui
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import de.mw136.tonuino.BulkEditViewModel
 import de.mw136.tonuino.R
+import de.mw136.tonuino.byteArrayToHex
+import de.mw136.tonuino.nfc.NfcIntentActivity
+import de.mw136.tonuino.nfc.TagData
+import de.mw136.tonuino.nfc.readFromTag
 
 import kotlinx.android.synthetic.main.bulkwrite_activity.*
 
 @ExperimentalUnsignedTypes
-class BulkWriteActivity : AppCompatActivity() {
+class BulkWriteActivity : NfcIntentActivity() {
+    override val TAG = "BulkActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,4 +40,14 @@ class BulkWriteActivity : AppCompatActivity() {
         }
     }
 
+
+    override fun onNfcTag(tag: Tag) {
+        Log.d(TAG, "onNfcTag ${byteArrayToHex(tag.id.toUByteArray())}")
+        val bytes = readFromTag(tag)
+        Log.d(TAG, "bytes: ${byteArrayToHex(bytes).joinToString(" ")}")
+
+        if (bytes.isNotEmpty()) {
+
+        }
+    }
 }
