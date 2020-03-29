ca3f2f52edf7953303fb01dfeec1333d1f5a97e3
Marc Walter <walter.marc@outlook.com>
2020-03-29 12:29:27 +0200

    Add writing of tags

--- app/src/main/java/de/mw136/tonuino/ui/EnterTagActivity.kt
+++ app/src/main/java/de/mw136/tonuino/ui/EnterTagActivity.kt
@@ -1,20 +1,33 @@
 package de.mw136.tonuino.ui
 
+import android.app.AlertDialog
+import android.nfc.FormatException
 import android.nfc.Tag
+import android.nfc.tech.TagTechnology
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import de.mw136.tonuino.R
-import de.mw136.tonuino.nfc.NfcIntentActivity
-import de.mw136.tonuino.ui.enter.EnterViewModel
+import de.mw136.tonuino.nfc.*
 import de.mw136.tonuino.ui.enter.EnterFragmentPagerAdapter
+import de.mw136.tonuino.ui.enter.EnterViewModel
+import java.io.IOException
 
 @ExperimentalUnsignedTypes
 class EnterTagActivity : NfcIntentActivity() {
     override val TAG = "EnterTagActivity"
 
     private val tagData: EnterViewModel by viewModels()
+
+    var tag: TagTechnology? = null
+    private lateinit var isTagConnected: Runnable
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +37,134 @@ class EnterTagActivity : NfcIntentActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+
+        isTagConnected = Runnable {
+            if (tag?.isConnected == true) {
+                // should be able to write to tag
+                pollTag()
+            } else {
+                tag = null
+                enableWriteButtonIfTagPresent()
+            }
+        }
+
+        intent.getParcelableExtra<Tag>(PARCEL_TAG)?.let { tag -> onNfcTag(tag) }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        pollTag()
+    }
+
+    private fun pollTag() {
+        handler.postDelayed(isTagConnected, 321)
+    }
+
+    private fun enableWriteButtonIfTagPresent() {
+        findViewById<Button>(R.id.button_write)?.apply {
+            if (this@EnterTagActivity.tag == null) {
+                setText(getString(R.string.edit_write_button_no_tag))
+                isEnabled = false
+                Toast.makeText(
+                    this@EnterTagActivity,
+                    getString(R.string.edit_nfc_connection_lost),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                setText(
+                    getString(
+                        R.string.edit_write_button,
+                        tagIdAsString(this@EnterTagActivity.tag!!)
+                    )
+                )
+                isEnabled = true
+                pollTag()
+            }
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onClickWriteTagButton(view: View) = writeTag()
+
+    private fun writeTag() {
+        var result = WriteResult.TAG_UNAVAILABLE
+        if (tag != null) {
+            Log.w("$TAG.writeTag", "will write to tag ${tagIdAsString(tag!!)}")
+            result = writeTonuino(tag!!, TagData(tagData.bytes))
+            Log.w("$TAG.writeTag", "result $result")
+        }
+        showModalDialog(result)
+    }
+
+    private fun showModalDialog(result: WriteResult) {
+        with(AlertDialog.Builder(this)) {
+            var showRetryButton = false
+
+            when (result) {
+                WriteResult.SUCCESS -> {
+                    setMessage(R.string.written_success)
+                }
+                WriteResult.UNSUPPORTED_FORMAT -> {
+                    setTitle(R.string.written_unsupported_tag_type)
+                    setMessage(
+                        getString(
+                            R.string.nfc_tag_technologies,
+                            techListOf(tag).joinToString(", ")
+                        )
+                    )
+                }
+                WriteResult.AUTHENTICATION_FAILURE -> {
+                    setTitle(R.string.written_title_failure)
+                    setMessage(R.string.written_authentication_failure)
+                    showRetryButton = true
+                }
+                WriteResult.TAG_UNAVAILABLE -> {
+                    setTitle(R.string.written_title_failure)
+                    setMessage(R.string.written_tag_unavailable)
+                    showRetryButton = true
+                }
+                WriteResult.UNKNOWN_ERROR -> {
+                    setTitle(R.string.written_unknown_error)
+                    setMessage(
+                        getString(
+                            R.string.nfc_tag_technologies,
+                            techListOf(tag).joinToString(", ")
+                        )
+                    )
+                    showRetryButton = true
+                }
+            }
+
+            setPositiveButton(getString(R.string.button_ok)) { _, _ -> }
+            if (showRetryButton) {
+                setNegativeButton(getString(R.string.written_button_retry)) { _, _ -> writeTag() }
+            }
+            create().show()
+        }
     }
 
     override fun onNfcTag(tag: Tag) {
-        TODO("Not yet implemented")
+        val tagId = tagIdAsString(tag)
+        Log.i("$TAG.onNfcTag", "Tag $tagId")
+//        supportActionBar?.title = getString(R.string.read_title, tagId)
+        try {
+            this.tag = connectTo(tag)
+            enableWriteButtonIfTagPresent()
+        } catch (ex: IOException) {
+            // could not connect to tag
+            Log.w("$TAG.onNfcTag", "Could not connect to the NFC tag")
+        } catch (ex: FormatException) {
+            // unsupported tag format
+            Log.w("$TAG.onNfcTag", "Unsupported format")
+        } catch (ex: Exception) {
+            // TODO display unexpected error
+            Log.e("$TAG.onNfcTag", ex.toString())
+        }
     }
-
 }
 
--- app/src/main/java/de/mw136/tonuino/ui/enter/EnterViewModel.kt
+++ app/src/main/java/de/mw136/tonuino/ui/enter/EnterViewModel.kt
@@ -14,50 +14,50 @@ private const val SPECIAL2 = 8
 
 @ExperimentalUnsignedTypes
 class EnterViewModel : ViewModel() {
-    var _bytes: UByteArray = EnterViewModel.default()
+    var bytes: UByteArray = EnterViewModel.default()
         private set
 
     val cookie: UByteArray
         get() {
-            return _bytes.sliceArray(COOKIE until VERSION)
+            return bytes.sliceArray(COOKIE until VERSION)
         }
 
     fun setCookie(value: UByteArray) {
         TODO("Implement setCookie")
     }
 
-    private var _version: MutableLiveData<UByte> = MutableLiveData(_bytes[VERSION])
+    private var _version: MutableLiveData<UByte> = MutableLiveData(bytes[VERSION])
     val version: LiveData<UByte> get() = _version
     fun setVersion(value: UByte) {
-        _bytes[VERSION] = value
+        bytes[VERSION] = value
         _version.value = value
     }
 
-    private var _folder: MutableLiveData<UByte> = MutableLiveData(_bytes[FOLDER])
+    private var _folder: MutableLiveData<UByte> = MutableLiveData(bytes[FOLDER])
     val folder: LiveData<UByte> get() = _folder
     fun setFolder(value: UByte) {
-        _bytes[FOLDER] = value
+        bytes[FOLDER] = value
         _folder.value = value
     }
 
-    private var _mode: MutableLiveData<UByte> = MutableLiveData(_bytes[MODE])
+    private var _mode: MutableLiveData<UByte> = MutableLiveData(bytes[MODE])
     val mode: LiveData<UByte> get() = _mode
     fun setMode(value: UByte) {
-        _bytes[MODE] = value
+        bytes[MODE] = value
         _mode.value = value
     }
 
-    private var _special: MutableLiveData<UByte> = MutableLiveData(_bytes[SPECIAL])
+    private var _special: MutableLiveData<UByte> = MutableLiveData(bytes[SPECIAL])
     val special: LiveData<UByte> get() = _special
     fun setSpecial(value: UByte) {
-        _bytes[SPECIAL] = value
+        bytes[SPECIAL] = value
         _special.value = value
     }
 
-    private var _special2: MutableLiveData<UByte> = MutableLiveData(_bytes[SPECIAL2])
+    private var _special2: MutableLiveData<UByte> = MutableLiveData(bytes[SPECIAL2])
     val special2: LiveData<UByte> get() = _special2
     fun setSpecial2(value: UByte) {
-        _bytes[SPECIAL2] = value
+        bytes[SPECIAL2] = value
         _special2.value = value
     }
 
